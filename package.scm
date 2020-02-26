aa1eb8f9386a97732065c5e8e5e1edfa1f7498b7
Justin Ethier <justin.ethier@gmail.com>
2020-02-25 21:53:37 -0500

    Initial file

---
+++ package.scm
@@ -0,0 +1,14 @@
+(package
+ (name           curl)
+ (version        0.1)
+ (license        "MIT")
+ (authors        "Justin Ethier")
+ (maintainers    "Justin Ethier <justin.ethier at gmail dot com>")
+ (description    "Bindings for the cURL library")
+ (tags           "networking" "url")
+ (docs           "https://github.com/cyclone-scheme/cyclone-winds/wiki/curl")
+ ;(test           "array-list-tests.scm")
+
+ (library
+     (name (cyclone curl))
+   (description "cURL library")))
